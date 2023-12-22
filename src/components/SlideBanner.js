116eed686e50a81f649a7025927964d37a5b2770
sseung <dptmejf34@gmail.com>
2023-12-23 08:31:53 +0900

    swiper option 변경

--- src/components/SlideBanner.js
+++ src/components/SlideBanner.js
@@ -1,17 +1,29 @@
 import {Swiper, SwiperSlide} from "swiper/react";
-import {Pagination} from "swiper";
+import {EffectCoverflow, Pagination} from "swiper";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "../styles/slidebanner.scss";
 
 function SlideBanner({list, addClass}){
+
     return (
         <Swiper
             className={addClass ? addClass : ''}
+            effect={'coverflow'}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={'auto'}
+            coverflowEffect={{
+                rotate: 50,
+                stretch: 0,
+                depth: 80,
+                modifier: 1,
+                slideShadows: false,
+            }}
             allowTouchMove
             pagination={{ clickable: true }}
-            modules={[Pagination]}
+            modules={[EffectCoverflow, Pagination]}
         >
             {list.map((item, idx) =>
                 <SwiperSlide key={idx}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,14 +23,34 @@ function Home(){
             <SlideBanner
                 list={[
                     {
-                        tit: "사이드 메뉴 50% OFF",
                         desc: "인기 사이드 모조리 반값!",
+                        tit: "사이드 메뉴 50% OFF",
                         img: "main_banner_01.png"
                     },
                     {
-                        tit:"새로운 와우박스",
+                        desc: "피자알볼로 X 제주맥주",
+                        tit:"제주 스페셜 에디션",
+                        img: "main_banner_02.png"
+                    },
+                    {
+                        desc: "라지 두판에 더블박스",
+                        tit:"두 판에 19,900원",
+                        img: "main_banner_03.png"
+                    },
+                    {
+                        desc: "Classic of Classic",
+                        tit:"웰빙 피자 스페셜",
+                        img: "main_banner_04.png"
+                    },
+                    {
                         desc: "피자와 사이드를 한번에",
+                        tit:"새로운 와우박스",
                         img: "main_banner_05.png"
+                    },
+                    {
+                        desc: "모든 프리미엄 피자 할인",
+                        tit:"포장도! 배달도!",
+                        img: "main_banner_06.png"
                     }
                 ]}
                 addClass="main-slider"
